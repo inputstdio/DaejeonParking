929842f28e26299f27fb8bc9f9d9d8f4bdcb7dce
inputstdio <input@stdio.pe.kr>
2018-12-11 10:24:23 +0900

    지도 풍선 클릭시 카카오 맵으로 이동 완성

--- app/src/main/java/input/stdio/pe/kr/daejeonparking/MainActivity.java
+++ app/src/main/java/input/stdio/pe/kr/daejeonparking/MainActivity.java
@@ -14,7 +14,6 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
--- app/src/main/java/input/stdio/pe/kr/daejeonparking/SearchActivity.java
+++ app/src/main/java/input/stdio/pe/kr/daejeonparking/SearchActivity.java
@@ -1,11 +1,14 @@
 package input.stdio.pe.kr.daejeonparking;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +18,7 @@ import android.widget.TextView;
 
 import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapReverseGeoCoder;
 import net.daum.mf.map.api.MapView;
 
 import java.util.ArrayList;
@@ -26,6 +30,7 @@ public class SearchActivity extends AppCompatActivity {
     private ParkingAdapter parkingAdapter;
     private TextView alert_textView;
     private MapView mapView;
+    private ParkingBean data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,25 +46,14 @@ public class SearchActivity extends AppCompatActivity {
         ListView listView = findViewById(R.id.listView);
         listView.setAdapter(parkingAdapter);
         listView.setOnItemClickListener(click_Item);
-
-        mapView = new MapView(this);
-        mapView.setDaumMapApiKey("801f6106ad17677e988f31d884406d79");
-
-/*        TextView textView = findViewById(R.id.text_view);
-        StringBuilder sb = new StringBuilder();
-        int cnt = 0;
-        for(ParkingBean data : parkingBeans){
-            cnt++;
-            sb.append(data.getAllString()).append("\n");
-        }
-        sb.insert(0, "검색된 주차장 수 : " + cnt + "개\n\n");
-        textView.setText(sb.toString());*/
     }
 
     ListView.OnItemClickListener click_Item = new AdapterView.OnItemClickListener() {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            ParkingBean data = (ParkingBean) parent.getAdapter().getItem(position);
+            mapView = new MapView(SearchActivity.this);
+            mapView.setDaumMapApiKey("801f6106ad17677e988f31d884406d79");
+            data = (ParkingBean) parent.getAdapter().getItem(position);
             LayoutInflater click_inflater = getLayoutInflater();
             View alertLayout = click_inflater.inflate(R.layout.alert_parking_info, null);
             AlertDialog.Builder alert = new AlertDialog.Builder(SearchActivity.this);
@@ -73,9 +67,11 @@ public class SearchActivity extends AppCompatActivity {
             alert_textView = alertLayout.findViewById(R.id.text_view);
             alert_textView.setText(data.getAllString());
             ViewGroup mapViewContainer = alertLayout.findViewById(R.id.map_view);
+
             MapPoint mapPoint = MapPoint.mapPointWithGeoCoord(Double.parseDouble(data.getLAT()), Double.parseDouble(data.getLON()));
             mapView.setMapCenterPoint(mapPoint, true);
             mapViewContainer.addView(mapView);
+            mapView.setPOIItemEventListener(poiItemEventListener);
 
             MapPOIItem marker = new MapPOIItem();
             marker.setItemName(data.getNAME());
@@ -86,6 +82,69 @@ public class SearchActivity extends AppCompatActivity {
             // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
             marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
             mapView.addPOIItem(marker);
+            MapReverseGeoCoder reverseGeoCoder = new MapReverseGeoCoder("801f6106ad17677e988f31d884406d79", mapPoint, reverseGeoCodingResultListener, SearchActivity.this);
+            reverseGeoCoder.startFindingAddress();
+        }
+    };
+
+    // 마커 아이템 리스너
+    MapView.POIItemEventListener poiItemEventListener = new MapView.POIItemEventListener() {
+        @Override
+        public void onPOIItemSelected(MapView mapView, MapPOIItem mapPOIItem) {
+
+        }
+
+        @Override
+        public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem) {
+            // 다음 지도(카카오맵) 로 연결
+            new AlertDialog.Builder(SearchActivity.this)
+//                        .setIcon(R.drawable.fix_icon) // 카카오 맵 아이콘 넣을 예정
+                    .setTitle("카카오 맵으로 이동 합니다.")
+                    .setNegativeButton("예", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            try {
+                                StringBuilder url = new StringBuilder("daummaps://look?p=");
+                                url.append(data.getLAT()).append(",").append(data.getLON());
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url.toString()));
+                                startActivity(intent);
+                            } catch (Exception e) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=net.daum.android.map"));
+                                startActivity(intent);
+                            }
+                        }
+                    })
+                    .setPositiveButton("아니오", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                        }
+                    })
+                    .setCancelable(false)
+                    .create()
+                    .show();
+        }
+
+        @Override
+        public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem, MapPOIItem.CalloutBalloonButtonType calloutBalloonButtonType) {
+
+        }
+
+        @Override
+        public void onDraggablePOIItemMoved(MapView mapView, MapPOIItem mapPOIItem, MapPoint mapPoint) {
+
+        }
+    };
+
+    // 좌표에서 주소 반환 리스너
+    MapReverseGeoCoder.ReverseGeoCodingResultListener reverseGeoCodingResultListener = new MapReverseGeoCoder.ReverseGeoCodingResultListener() {
+        @Override
+        public void onReverseGeoCoderFoundAddress(MapReverseGeoCoder mapReverseGeoCoder, String s) {
+            Log.d("Log", "Address : " + s);
+        }
+
+        @Override
+        public void onReverseGeoCoderFailedToFindAddress(MapReverseGeoCoder mapReverseGeoCoder) {
+            Log.d("Log", "no Address");
         }
     };
 
